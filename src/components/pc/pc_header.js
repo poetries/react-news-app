fb0ceed642e63bc8a1b6d0be59a6891ade2412f1
poetries <jingguanliuye@gmail.com>
2017-11-21 11:09:21 +0800

    '成pc_header mobile_header简简单布局

--- src/components/pc/pc_header.js
+++ src/components/pc/pc_header.js
@@ -9,14 +9,13 @@ import {
 	Input,
 	Button,
 	CheckBox,
-    Modal,
-    Link
+  Modal
 } from 'antd';
 const FormItem = Form.Item;
 const SubMenu = Menu.SubMenu;
 const TabPane = Tabs.TabPane;
 const MenuItemGroup = Menu.ItemGroup;
-
+import {Router, Route, Link, browserHistory} from 'react-router'
 
 class PCHeader extends React.Component{
     state = {
@@ -52,28 +51,33 @@ class PCHeader extends React.Component{
         }
         const formData = this.props.form.getFieldsValue();
         // console.log(formData);
-        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=" + this.state.action
-		+ "&username="+formData.userName+"&password="+formData.password
-		+"&r_userName=" + formData.r_userName + "&r_password="
-		+ formData.r_password + "&r_confirmPassword="
-		+ formData.r_confirmPassword, myFetchOptions)
-		.then(response => response.json())
-		.then(json => {
-			this.setState({userNickName: json.NickUserName, userid: json.UserId});
-		});
-		if (this.state.action=="login") {
-			this.setState({hasLogined:true});
-		}
-		message.success("请求成功！");
-		this.setModalVisible(false);
+          fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=" + this.state.action
+              + "&username="+formData.userName+"&password="+formData.password
+              +"&r_userName=" + formData.r_userName + "&r_password="
+              + formData.r_password + "&r_confirmPassword="
+              + formData.r_confirmPassword, myFetchOptions)
+              .then(response => response.json())
+              .then(json => {
+                this.setState({userNickName: json.NickUserName, userid: json.UserId});
+              });
+              if (this.state.action=="login") {
+                this.setState({hasLogined:true});
+              }
+              message.success("请求成功！");
+              this.setModalVisible(false);
+    }
+    callback(key) {
+      if (key == 1) {
+        this.setState({action: 'login'});
+      } else if (key == 2) {
+        this.setState({action: 'register'});
+      }
     }
     render(){
-        let {getFieldProps} = this.props.form;
+        let {getFieldDecorator} = this.props.form;
         const userShow = this.state.hasLogined?
-            <Menu.Item key='logout' className='register'>
-                <Button type='primary' htmlType='button'>{this.props.userNickName}</Button>
-                &nbsp;&nbsp;
-                <Link>
+            <Menu.Item key='logout' className='register' style={{color:'#fff',borderBottom:'none'}}>
+                <Link to='/'>
                     <Button type='dashed' htmlType='button'>个人中心</Button>
                 </Link>
                 &nbsp;&nbsp;
@@ -121,18 +125,37 @@ class PCHeader extends React.Component{
                         {userShow}
                     </Menu>
                     <Modal title="用户中心" wrapClassName="vertical-center-modal" visible={this.state.modalVisible} onCancel= {()=>this.setModalVisible(false)} onOk={() => this.setModalVisible(false)} okText="关闭">
-                       <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
-                            <FormItem label='账户'>
-                                    <Input placeholder='请输入您的账号' {...getFieldProps('r_userName')}/>
-                                </FormItem>
-                                <FormItem label='密码'>
-                                    <Input type='password' placeholder='请输入您的密码' {...getFieldProps('r_password')}/>
-                                </FormItem>
-                                <FormItem label='确认密码'>
-                                    <Input type='password'  placeholder='请再次输入您的密码' {...getFieldProps('r_confirmPassword')}/>
-                            </FormItem>
-                            <Button type='primary' htmlType='submit'>注册</Button>
-                       </Form>
+
+                       <Tabs defaultActiveKey="1" onChange={this.callback.bind(this)}>
+                           <TabPane tab="登录" key="1">
+                              <Form  layout="horizontal" onSubmit={this.handleSubmit.bind(this)}>
+                                  <FormItem label='账户'>
+                                          <Input placeholder='请输入您的账号' {...getFieldDecorator('r_userName')}/>
+                                      </FormItem>
+                                      <FormItem label='密码'>
+                                          <Input type='password' placeholder='请输入您的密码' {...getFieldDecorator('r_password')}/>
+                                      </FormItem>
+                                      <FormItem label='确认密码'>
+                                          <Input type='password'  placeholder='请再次输入您的密码' {...getFieldDecorator('r_confirmPassword')}/>
+                                  </FormItem>
+                                  <Button type='primary' htmlType='submit'>登录</Button>
+                              </Form>
+                           </TabPane>
+                           <TabPane tab="注册" key="2">
+                              <Form  layout="horizontal" onSubmit={this.handleSubmit.bind(this)}>
+                                  <FormItem label='账户'>
+                                          <Input placeholder='请输入您的账号' {...getFieldDecorator('r_userName')}/>
+                                      </FormItem>
+                                      <FormItem label='密码'>
+                                          <Input type='password' placeholder='请输入您的密码' {...getFieldDecorator('r_password')}/>
+                                      </FormItem>
+                                      <FormItem label='确认密码'>
+                                          <Input type='password'  placeholder='请再次输入您的密码' {...getFieldDecorator('r_confirmPassword')}/>
+                                  </FormItem>
+                                  <Button type='primary' htmlType='submit'>注册</Button>
+                              </Form>
+                           </TabPane>
+                       </Tabs>
                     </Modal>
                 </Col>
                 <Col span={2}></Col>
