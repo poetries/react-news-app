ebff51cc3a925213f3315441a956755b06ec5c59
poetries <jingguanliuye@gmail.com>
2017-11-22 18:12:06 +0800

    update

---
+++ src/components/elements/Tags.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// 结构化
+export const Article = styled.article``;
+export const Aside = styled.aside``;
+export const Footer = styled.footer``;
+export const Header = styled.header``;
+export const Section = styled.section``;
+export const Nav = styled.nav``;
+
+export const H1 = styled.h1``;
+export const H2 = styled.h2``;
+export const H3 = styled.h3``;
+export const H4 = styled.h4``;
+export const H5 = styled.h5``;
+export const H6 = styled.h6``;
+
+export const Div = styled.div``;
+export const TagP = styled.p``;
+export const Span = styled.span``;
+
+// 表单
+export const Fieldset = styled.fieldset``;
+export const Legend = styled.legend``;
+export const Form = styled.form``;
+export const Input = styled.input``;
+export const Label = styled.label``;
+export const Optgroup = styled.optgroup``;
+export const Select = styled.select``;
+export const Textarea = styled.textarea``;
+
+// 按钮、链接与输出
+export const TagA = styled.a``;
+export const Button = styled.button``;
+export const Output = styled.output``;
+
+// 表格
+export const Caption = styled.caption``;
+export const Table = styled.table``;
+export const Tfoot = styled.tfoot``;
+export const Th = styled.th``;
+export const Thead = styled.thead``;
+export const Tr = styled.tr``;
+export const Tbody = styled.tbody``;
+export const Td = styled.td``;
+
+// 列表
+export const Dl = styled.dl``;
+export const Dt = styled.dt``;
+export const Dd = styled.dd``;
+export const Ol = styled.ol``;
+export const Ul = styled.ul``;
+export const Li = styled.li``;
+
+// 语义化文本
+export const Abbr = styled.abbr``;
+export const Address = styled.address``;
+
+export const Code = styled.code``;
+export const Pre = styled.pre``;
+
+export const Blockquote = styled.blockquote``;
+export const Cite = styled.cite``;
+export const TagQ = styled.q``;
+
+export const Del = styled.del``;
+export const Em = styled.em``;
+export const TagU = styled.u``; 
+export const Small = styled.small``;
+export const Strong = styled.strong``;
+export const Sub = styled.sub``;
+export const Sup = styled.sup``;
+
+// 多媒体及插件
+export const Audio = styled.audio``;
+export const Embed = styled.embed``;
+export const Figure = styled.figure``;
+export const Figcaption = styled.figcaption``;
+export const Img = styled.img``;
+export const Video = styled.video``;
+export const Source = styled.source``;
+
+// 其他（建议只在需要空标签时使用）
+export const TagB = styled.b``; // strong
+export const TagI = styled.i``; // em
+
+// area
+// base, body, br
+// canvas, col, colgroup
+// data, datalist
+// details, dfn
+// head, hr, html
+// iframe, ins
+// kbd
+// link
+// main, map, mark, meta, meter
+// noembed, noframes, noscript, object
+// process
+// rp, rt, rtc, ruby
+// s, samp, script, strike, style, summary
+// template, time, title, track, tt
+
+
